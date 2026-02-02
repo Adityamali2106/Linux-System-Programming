b6c11d17d7996efd898114aed5b846ea16fd75c2
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-02-02 21:18:25 +0530

    Add header comments to LibMystrlen.c
    Added header comments to the LibMystrlen.c file, including program name, description, author, and date.

--- Assignments/Assignment07/Program07_4/LibMystrlen.c
+++ Assignments/Assignment07/Program07_4/LibMystrlen.c
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//      Program Name : LibMystrlen
+//      Description  : This is a libarary file which is used to calculate length of a string
+//      Author       : Aditya Vijay Mali
+//      Date         : 02/02/2026
+//
+//////////////////////////////////////////////////////////////////////////////////
+
 int Mystrlen(char *str)
 {
     int i = 0;
