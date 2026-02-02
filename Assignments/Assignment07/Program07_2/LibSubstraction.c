280ec25cc58d39d3cb55a9cd2cbe0f530adaa693
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-02-02 21:15:57 +0530

    Create LibSubstraction.c for subtraction functionality
    Added a library function for subtraction.

--- Assignments/Assignment07/Program07_2/LibSubstraction.c
+++ Assignments/Assignment07/Program07_2/LibSubstraction.c
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//      Program Name : LibSubstraction
+//      Description  : This program file is a libarary file which used to calculate subtraction of two numbers
+//      Author       : Aditya Vijay Mali
+//      Date         : 02/02/2026
+//
+//////////////////////////////////////////////////////////////////////////////////
+
 int Substraction(int A, int B)
 {
     return A - B;
